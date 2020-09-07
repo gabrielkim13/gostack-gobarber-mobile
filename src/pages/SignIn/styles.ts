83d69e160dd4a6f9d27e971d0a97be0bb4dcb198
Gabriel Kim <gabrielkim13@gmail.com>
2020-09-07 16:55:24 -0300

    Add SignIn page

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
   flex: 1;
@@ -14,4 +15,37 @@ export const Title = styled.Text`
   color: #f4ede8;
 
   margin: 64px 0 24px;
+`;
+
+export const ForgotPassword = styled.TouchableOpacity`
+  margin-top: 24px;
+`;
+
+export const ForgotPasswordText = styled.Text`
+  font-family: 'RobotoSlab-Regular';
+  font-size: 16px;
+  color: #f4ede8;
+`;
+
+export const CreateAccountButton = styled.TouchableOpacity`
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+
+  background: #312e38;
+  border-top-width: 1px;
+  border-color: #232129;
+  padding: 16px 0 ${16 + getBottomSpace()}px;
+
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const CreateAccountButtonText = styled.Text`
+  font-family: 'RobotoSlab-Regular';
+  font-size: 18px;
+  color: #ff9900;
+  margin-left: 16px;
 `;
