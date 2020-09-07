ed09b2d105c9c4fdf6d87c0a30386461aa6b9add
Gabriel Kim <gabrielkim13@gmail.com>
2020-09-07 15:55:34 -0300

    Add Roboto Slab fonts

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Image } from 'react-native';
+
+import logoImg from '../../assets/logo.png';
+
+import { Container, Title } from './styles';
 
 const SignIn: React.FC = () => {
-  return <View />;
+  return (
+    <Container>
+      <Image source={logoImg} />
+
+      <Title>Faça seu login</Title>
+    </Container>
+  );
 };
 
 export default SignIn;
